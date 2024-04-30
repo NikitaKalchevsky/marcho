72cf1078e4bb67a1f96579c426a225b64bb02cbd
NikitaKalchevsky <mazdozavr@gmail.com>
2024-04-30 22:24:31 +0300

    slider

---
+++ app/js/main.js
@@ -0,0 +1,10 @@
+$(".top-slider__inner").slick({
+  dots: true,
+  infinite: true,
+  arrows: false,
+  speed: 500,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+});
